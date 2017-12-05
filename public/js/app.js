6059b117816fb2e46bd02048556b75326b9f2456
geronimo-lisboa <erikaeluciano2016@gmail.com>
2017-12-05 20:00:48 -0200

    comentarios e algumas modificações

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,17 @@
-class ProductList extends React.Component {
+//Isso aqui é um componente do react, herdeiro de React.Component.
+//O unico método obrigatório é o render().
+class Foobar extends React.Component {
   render() {
     return (
       <div className='ui unstackable items'>
-        Hello world
+        Hello World
       </div>
     );
   }
 }
-
+//É a renderizaçaõ do DOM do react na página. O componte Foobar, definido pela
+//classe Foobar será renderizado no componente de id = content.
 ReactDOM.render(
-  <ProductList />,
+  <Foobar />,
   document.getElementById('content')
 );
